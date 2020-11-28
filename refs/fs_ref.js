50493d83328cbed419ff3d6c603e4fcc7e557bab
unknown <olysiuk01@gmail.com>
2020-11-28 20:20:25 +0200

    fs(filesystem) module - mkdir()

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -1,2 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 
+// fs.mkdir(path.join(__dirname, 'notes'), (err) => {
+//   if (err) throw err
+
+//   console.log('Folder was created')
+// })

7bc856d9a242417e284541bbd714c18cbcabb5fc
unknown <olysiuk01@gmail.com>
2020-11-28 22:12:53 +0200

    os(operationsystem) module - cpus

--- refs/os_ref.js
+++ refs/os_ref.js
@@ -1,8 +1,12 @@
+const { log } = require('console')
 const os = require('os')
 
 // Платформа
 console.log(os.platform())
 
-
 // Архитектура
 console.log(os.arch())
+
+// Информация
+
+console.log(os.cpus())

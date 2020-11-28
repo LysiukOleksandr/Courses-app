5ce650c7d4701c8ef3fe907fbdbb2a22b19fda71
unknown <olysiuk01@gmail.com>
2020-11-29 00:24:00 +0200

    http module - write,end

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const http = require('http')
 
+const server = http.createServer((req, res) => {
+  console.log(req.url)
 
-const server = http.createServer((req,res)=>{
-  
+  res.write('<h1>Hello from Node.js</h1>')
+  res.write('<h2>Hello from Node.js</h2>')
+  res.write('<h3>Hello from Node.js</h3>')
+  res.end(`<div style="background: red; width: 200px; height: 200px;">
+  <h1>Test</h1>
+  </div>`)
 })
 
-
-server.listen(3000,()=>{
+server.listen(3000, () => {
   console.log('Server is running')
 })

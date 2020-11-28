f7afb841834a1fbf7967dc463e891a5be19b4eb8
unknown <olysiuk01@gmail.com>
2020-11-28 21:43:27 +0200

    fs(filesystem) module - writeFile,appendFile

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -6,3 +6,21 @@ const path = require('path')
 
 //   console.log('Folder was created')
 // })
+
+fs.writeFile(
+  path.join(__dirname, 'notes', 'mynotes.txt'),
+  'Hello world',
+  (err) => {
+    if (err) throw err
+    console.log('File was created')
+
+    fs.appendFile(
+      path.join(__dirname, 'notes', 'mynotes.txt'),
+      'From append file',
+      (err) => {
+        if (err) throw err
+        console.log('File was changed')
+      }
+    )
+  }
+)

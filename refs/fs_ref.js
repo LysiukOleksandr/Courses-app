2b3ff2c91d645b2ea0920338a2ce7b2d4a33ad79
unknown <olysiuk01@gmail.com>
2020-11-28 22:04:57 +0200

    fs(filesystem) module - rename

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -7,25 +7,37 @@ const path = require('path')
 //   console.log('Folder was created')
 // })
 
-fs.writeFile(
+// fs.writeFile(
+//   path.join(__dirname, 'notes', 'mynotes.txt'),
+//   'Hello world',
+//   (err) => {
+//     if (err) throw err
+//     console.log('File was created')
+
+//     fs.appendFile(
+//       path.join(__dirname, 'notes', 'mynotes.txt'),
+//       'From append file',
+//       (err) => {
+//         if (err) throw err
+//         console.log('File was changed')
+//       }
+//     )
+//     fs.readFile(
+//       path.join(__dirname, 'notes', 'mynotes.txt'),
+//       'utf-8',
+//       (err, data) => {
+//         if (err) throw err
+//         console.log(data)
+//       }
+//     )
+//   }
+// )
+
+fs.rename(
   path.join(__dirname, 'notes', 'mynotes.txt'),
-  'Hello world',
+  path.join(__dirname, 'notes', 'notes.txt'),
   (err) => {
     if (err) throw err
-    console.log('File was created')
-
-    fs.appendFile(
-      path.join(__dirname, 'notes', 'mynotes.txt'),
-      'From append file',
-      (err) => {
-        if (err) throw err
-        console.log('File was changed')
-      }
-    )
+    console.log('File was renamed')
   }
 )
-
-fs.readFile(path.join(__dirname, 'notes', 'mynotes.txt'), (err, data) => {
-  if (err) throw err
-  console.log(data)
-})

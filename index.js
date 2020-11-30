7e1cc0ca0e56fd9b3fb654d98fce19008a13d56d
unknown <olysiuk01@gmail.com>
2020-11-30 23:28:11 +0200

    sendfile express

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running by port ${PORT}`)
 })

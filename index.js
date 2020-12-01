8fa370f8dc4f94762432bbb71cf50d5f2484aa22
unknown <olysiuk01@gmail.com>
2020-12-01 21:59:18 +0200

    handlebars and layout + materialize css

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
-let app = express()
+const app = express()
 
-const PORT = process.env.PORT || 3000
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'home.html'))
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+  res.render('about')
 })
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running by port ${PORT}`)

46cd4082d4f638cff71d820736ba356f1861885f
unknown <olysiuk01@gmail.com>
2020-12-01 23:53:28 +0200

    routes registration

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
-
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 const app = express()
 
 const hbs = exphbs.create({
@@ -14,26 +16,9 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Главная страница',
-    isHome: true,
-  })
-})
-
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Добавить курс',
-    isAdd: true,
-  })
-})
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Курсы',
-    isCourses: true,
-  })
-})
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 const PORT = process.env.PORT || 3000
 

4bd0ecf420cca8c957585683e04e45f9c8c9edae
unknown <olysiuk01@gmail.com>
2020-12-02 23:26:15 +0200

    finished cart

--- public/app.js
+++ public/app.js
@@ -1,8 +1,13 @@
+
+  toCurrency = price =>{
+    return new Intl.NumberFormat('ru-RU', {
+      currency: 'rub',
+      style: 'currency',
+    }).format(price)
+  }
+
 document.querySelectorAll('.price').forEach((node) => {
-  node.textContent = new Intl.NumberFormat('ru-RU', {
-    currency: 'rub',
-    style: 'currency',
-  }).format(node.textContent)
+  node.textContent = toCurrency(node.textContent)
 })
 
 const $cart = document.querySelector('#cart')
@@ -16,7 +21,25 @@ if ($cart) {
       })
         .then((res) => res.json())
         .then((cart) => {
-          console.log(cart)
+          if (cart.courses.length) {
+            const html = cart.courses
+              .map((c) => {
+                return `
+              <tr>
+              <td>${c.title}</td>
+              <td>${c.count}</td>
+              <td>
+                <button class="btn btn-small js-remove" data-id="${c.id}">Удалить</button>
+              </td>
+            </tr>
+              `
+              })
+              .join('')
+            $cart.querySelector('tbody').innerHTML = html
+            $cart.querySelector('.price').textContent = toCurrency(cart.price)
+          } else {
+            $cart.innerHTML = '<p>Корзина пуста</p>'
+          }
         })
     }
   })

12df295c4e74230965fd1f29f01f6234301469db
unknown <olysiuk01@gmail.com>
2020-12-05 23:42:19 +0200

    handlebars order

--- public/app.js
+++ public/app.js
@@ -1,13 +1,27 @@
+toCurrency = (price) => {
+  return new Intl.NumberFormat('ru-RU', {
+    currency: 'rub',
+    style: 'currency',
+  }).format(price)
+}
 
-  toCurrency = price =>{
-    return new Intl.NumberFormat('ru-RU', {
-      currency: 'rub',
-      style: 'currency',
-    }).format(price)
-  }
+const toDate = (date) => {
+  return new Intl.DateTimeFormat('ru-RU', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(new Date(date))
+}
 
 document.querySelectorAll('.price').forEach((node) => {
   node.textContent = toCurrency(node.textContent)
+})
+
+document.querySelectorAll('.date').forEach((node) => {
+  node.textContent = toDate(node.textContent)
 })
 
 const $cart = document.querySelector('#cart')

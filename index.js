b0e84465791e0029c1db00be790c0f70551f4acd
unknown <olysiuk01@gmail.com>
2020-12-05 14:31:11 +0200

    courses mongoDB

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require('express')
 const path = require('path')
+const Handlebars = require('handlebars')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cartRoutes = require('./routes/cart')
+const {
+  allowInsecurePrototypeAccess,
+} = require('@handlebars/allow-prototype-access')
 
 const app = express()
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+mongoose.set('useFindAndModify', false)
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
